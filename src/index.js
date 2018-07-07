ea1f6567dc393007897ba346997624713aaedf53
Tim Göller <tim.goeller@outlook.de>
2018-07-07 20:58:22 +0200

    Open modal window on "Take Note" button click
    Load the content of addNote.html.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const remote = require('electron').remote;
+const electron = require('electron');
+const BrowserWindow = electron.remote.BrowserWindow
 
 document.getElementById("quit-application-button").addEventListener("click", function quitAppListener() {
     remote.getCurrentWindow().close();
+})
+
+document.getElementById("addNoteButton").addEventListener('click', function addNoteButtonListener() {
+    //var addNoteWindow = window.open("addNote.html");
+    var win = new BrowserWindow({ parent:remote.getCurrentWindow(), modal: true, width: 350, height: 500 , frame: false});
+    win.on('close', function() {win = null});
+    
+    win.loadURL('file://' + __dirname + '/addNote.html');
+
+    win.show();
+    win.openDevTools();
 })
