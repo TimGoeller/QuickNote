c6246b41d486932571acf513d0c02aed28b97057
Tim Göller <tim.goeller@outlook.de>
2018-07-10 20:54:20 +0200

    Add new note title to list

--- src/index.js
+++ src/index.js
@@ -9,7 +9,19 @@ document.getElementById("add-note-button").addEventListener('click', function ad
 })
 
 ipcRenderer.on('attach-new-note', function attachNewNoteIPC(event, arg) {
-    let p = document.createElement("p")
-    p.innerHTML = arg.title
-    document.getElementById('note-sidebar').appendChild(p)
+    let noteEntryDiv = document.createElement("div")
+    let noteTitleDiv = document.createElement('div')
+    let noteOptionsDiv = document.createElement('div')
+    let noteTitleParagraph = document.createElement('p')
+
+    noteTitleParagraph.innerHTML = arg.title;
+    noteEntryDiv.setAttribute('id', 'note-entry')
+    noteTitleDiv.setAttribute('id', 'note-title')
+    noteOptionsDiv.setAttribute('id', 'note-options')
+
+    noteTitleDiv.appendChild(noteTitleParagraph);
+    noteEntryDiv.appendChild(noteTitleDiv);
+    noteEntryDiv.appendChild(noteOptionsDiv);
+
+    document.getElementById('note-sidebar').appendChild(noteEntryDiv)
 })
